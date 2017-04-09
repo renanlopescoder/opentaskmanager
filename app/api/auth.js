515cb98fe1cd34ea9d8140a9f39990b631c5503c
Renan Lopes <renanlopescoder@gmail.com>
2017-04-09 13:00:33 -0300

    refactor: API auth
    - Updated to ES6 patterns (Arrow function and scope variables)
    - Added initial documentation to auth API (Portuguese)

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,80 +1,94 @@
-  var mongoose =  require('mongoose');
-    
-  //--> Segurança
-  //--> Security
-
-  //--> Bcrypt para criptografar a senha do usuário
-  //--> Bcrypt to encrypt user's password
-
-  var bcrypt = require('bcrypt');
+  let mongoose =  require('mongoose');
+  let bcrypt = require('bcrypt');
   const saltRounds = 15;
-  
-  //--> Autenticação por Token
-  //--> Token Authentication
-  var jwt = require('jsonwebtoken');
+  let jwt = require('jsonwebtoken');
 
-  module.exports = function(app) {
+/**
+ * Auth API para gerenciamento de autenticação dos usuários no sistema
+ * 
+ * Requires: Json Web Token, simple-encryptor, bcrypt e mongoose
+ * Descrição: simple-encrytor usado para criptografar dados do usuário em localStorage e Json Web Token para autenticação do usuário no sistema.
+ *  
+ * Modelo: user.js
+ * Nome: User
+ * Collection no MongoDB: users
+ * Atualização: EcmaScript 6
+ * Conceitos Aplicados: Criptografia unidirecional e bidirecional Arrow Functions e Variáveis com Escopo de Bloco.
+ * 
+ */
 
-  var api = {};
-  var model = mongoose.model('User');
+
+  module.exports = (app) => {
+
+  let api = {};
+  let model = mongoose.model('User');
 
   api.login = function(req,res){
-    console.log(req.body.username);
-    console.log(req.body.password);
+    // console.log(req.body.username);
+    // console.log(req.body.password);
     
     model.findOne({username: req.body.username})
-    .then(function(user){
-      bcrypt.compare(req.body.password, user.password).then(function(result){
-        if(result == true){
-          var token = jwt.sign({user:user.username}, app.get('secret'),{
-            expiresIn: 84600
-          });
-          console.log('Token criado e sendo enviado no header http');
-          return res.status(200).json({nome: user.username, token: token, user_id: user._id});
+    .then(
+      (user) => {
+        bcrypt.compare(req.body.password, user.password).then(function(result){
+          if(result == true){
+            var token = jwt.sign({user:user.username}, app.get('secret'),{
+              expiresIn: 84600
+            });
+            // console.log('Token criado e sendo enviado no header http');
+            return res.status(200).json({nome: user.username, token: token, user_id: user._id});
         }
-      }, function(error){
+      }, 
+      (error) => {
         console.log('Error, password incorreto para o usuário ' + user.username);
         res.sendStatus(401);        
       });
-    },function(error){
-      console.log('Error, usuário inexistente');
-      res.sendStatus(401);
+    },
+      (error) => {
+        console.log('Error, usuário inexistente');
+        res.sendStatus(401);
     })
 
   };
 
 
-  api.createUser = function(req,res){
-    bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+  api.createUser = (req,res) => {
+    bcrypt.hash(req.body.password, saltRounds, (err,hash) => {
       req.body.password = hash;
-      model.create(req.body).then(function(user){
-      res.json(user);
-    }, function(error){
-        console.log(error);
-        res.status(404).json(error);
+      model.create(req.body).then(
+        (user)=> {
+          res.json(user);
+        },
+        (error) => {
+          console.log(error);
+          res.status(404).json(error);
       });
     });
   };
 
 
 //next so podemos usar com use deixar usar o middleware
-  api.verificaToken = function(req, res, next){
-    var token = req.get('Autorization');
-    if(token){
-          console.log('Verificando Token');
-      jwt.verify(token, app.get('secret'), function(error, decoded){
-        // if(error){
-        //   console.log('Token Rejeitado');
-        //   res.sendStatus(401);
-        // }
-        req.user = decoded;
-        console.log('Usuario aprovado');
-        next();
-      });
-    } else{
-      console.log('Token não enviado');
-      res.sendStatus(401);
-    }
+  api.verificaToken = (req, res, next) => {
+    let token = req.get('Autorization');
+      if(token) {
+        console.log('Verificando Token');
+        jwt.verify(
+          token, 
+          app.get('secret'), 
+          (error, decoded) => {
+            if(error) {
+              console.log('Token Rejeitado');
+              res.sendStatus(401);
+            }
+            req.user = decoded;
+            console.log('Usuario aprovado');
+            next();
+          }
+        );
+      } else {
+        console.log('Token não enviado');
+        res.sendStatus(401);
+      }
   };
   return api;
 };
